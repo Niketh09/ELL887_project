35ee70774c844ffbebb439ac870489d6b0a43a9d
Niketh09 <77853738+Niketh09@users.noreply.github.com>
2024-05-09 20:40:42 +0530

    Update script_list.js

--- script_list.js
+++ script_list.js
@@ -52,7 +52,7 @@ function displayProducts(products) {
 }
 
 function fetchProducts() {
-    fetch('https://foodcatalogue.azurewebsites.net/api/listProducts') // Assuming this endpoint is configured on your backend server
+    fetch('https://ell887userbackend.azurewebsites.net/api/listProducts') // Assuming this endpoint is configured on your backend server
         .then(response => {
             if (!response.ok) {
                 throw new Error('Failed to fetch products');
@@ -69,7 +69,7 @@ function fetchProducts() {
 }
 
 function fetchFilterOptions() {
-    fetch('https://foodcatalogue.azurewebsites.net/api/filterOptions')
+    fetch('https://ell887userbackend.azurewebsites.net/api/filterOptions')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Failed to fetch filter options');
@@ -241,7 +241,7 @@ function applyFilters() {
 
     // Now you can send the selectedFilters object to the backend for processing
     // Example fetch call:
-    fetch('https://foodcatalogue.azurewebsites.net/api/filterProducts', {
+    fetch('https://ell887userbackend.azurewebsites.net/api/filterProducts', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
